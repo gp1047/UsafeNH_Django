c200262ae7d67d5b675445da4a3ad6b6ef515cf8
Nick Bielinski <ndbielinski@gmail.com>
2017-02-22 21:14:08 -0500

    updated plugins

--- uSafeNH2/platforms/ios/platform_www/cordova_plugins.js
+++ uSafeNH2/platforms/ios/platform_www/cordova_plugins.js
@@ -92,6 +92,31 @@ module.exports = [
         "merges": [
             "Contact"
         ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -99,7 +124,10 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.3.1",
     "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-contacts": "2.2.1"
+    "cordova-plugin-contacts": "2.2.1",
+    "cordova-plugin-dialogs": "1.3.1",
+    "cordova-plugin-inappbrowser": "1.6.1",
+    "cordova-plugin-statusbar": "2.2.1"
 };
 // BOTTOM OF METADATA
 });
